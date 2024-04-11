8b530c81faa9afa6249aa327bc334d10a06c1ae1
KISMAT1234 <kismatbohora123@gmail.com>
2024-04-11 21:58:49 +0545

    database connection with mongodb

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+
+type ConnectionObject = {
+    isConnected?:number
+}
+
+const connection: ConnectionObject = {}
+
+async function dbConnect(): Promise<void> {
+    if(connection.isConnected){
+        console.log("Already connected to database")
+        return 
+    }
+
+    try{
+       const db = await mongoose.connect(process.env.MONGODB_URI || '', {})
+
+       connection.isConnected = db.connections[0].readyState
+       
+       console.log("Connection successful")
+    } catch(err){
+        console.log("connection database failed",err);
+        process.exit(1)
+    }
+
+}
+
+export default dbConnect;
