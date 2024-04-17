a7dd9e85bb69b72b3607ac376b6cfc25f7c0a4e1
KISMAT1234 <kismatbohora123@gmail.com>
2024-04-17 20:55:54 +0545

    username verified added in session

--- src/app/api/auth/[...nextauth]/option.ts
+++ src/app/api/auth/[...nextauth]/option.ts
@@ -60,9 +60,12 @@ export const authOptions: NextAuthOptions = {
       async session({ session,token }) {
         if(token){
           session.user._id = token._id
+          session.user.isVerified = token.isVerified
+          session.user.isAcceptingMessages = token.isAcceptingMessages
+          session.user.username = token.username
         }
+        return session
       },
-      return session
     },
     pages:{
       signIn: '/sign-in'
