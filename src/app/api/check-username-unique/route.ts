6ff80699808ce9e767742986d8900c629db364b9
KISMAT1234 <kismatbohora123@gmail.com>
2024-04-21 23:02:04 +0545

    getting value from query for validation

--- src/app/api/check-username-unique/route.ts
+++ src/app/api/check-username-unique/route.ts
@@ -3,3 +3,28 @@ import UserModel from "@/model/User";
 import {z} from "zod"
 import { usernameValidation } from "@/schemas/signupSchema";
 
+const UsernameQuerySchema = z.object({
+    username: usernameValidation
+})
+
+export async function GET(request: Request){
+    await dbConnect()
+    try{
+      const {searchParams} = new URL(request.url)
+      const queryParam = {
+        username: searchParams.get('username')
+
+      }
+
+
+    }
+    catch(err){
+        console.log(err);
+        return Response.json({success:false,message:"Error checking username"},
+        {status:500}
+        )
+    }
+}
+
+
+
