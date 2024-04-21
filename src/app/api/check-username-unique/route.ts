943dff447681a7f04df7d62da7269c478d6de665
KISMAT1234 <kismatbohora123@gmail.com>
2024-04-21 23:04:58 +0545

    finding user after success of validation and checking existing user or not

--- src/app/api/check-username-unique/route.ts
+++ src/app/api/check-username-unique/route.ts
@@ -30,6 +30,27 @@ export async function GET(request: Request){
         })
      }
 
+     const {username} = result.data
+
+     const existingVerifiedUser = await UserModel.findOne({ username, isVerified: true})
+
+     if(existingVerifiedUser){
+        return Response.json({
+            success: false,
+            message: 'Username is already taken'
+        },
+        {
+            status:400
+        })
+     }
+
+     return Response.json({
+        success: true,
+        message: 'Username is unique',
+    },
+    {
+        status:200
+    })
 
     }
     catch(err){
