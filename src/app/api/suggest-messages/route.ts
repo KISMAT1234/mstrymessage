5486b16b019cf68e1a8c6823b74f8a4068c5b3fe
KISMAT1234 <kismatbohora123@gmail.com>
2024-04-23 21:42:04 +0545

    ai integrated succcessfully

---
+++ src/app/api/suggest-messages/route.ts
@@ -0,0 +1,41 @@
+import OpenAI from 'openai';
+import { OpenAIStream, StreamingTextResponse } from 'ai';
+import { NextResponse } from 'next/server';
+ 
+// Create an OpenAI API client (that's edge friendly!)
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+ 
+export const dynamic = 'force-dynamic';
+ 
+export async function POST(req: Request) {
+    try{
+//   const { messages } = await req.json();
+     const prompt = "Create a list of three messages"
+ 
+  // Ask OpenAI for a streaming chat completion given the prompt
+  const response = await openai.completions.create({
+    model: 'gpt-3.5-turbo-instruct',
+    max_tokens: 400,
+    stream: true,
+    prompt,
+  });
+ 
+  // Convert the response into a friendly text-stream
+  const stream = OpenAIStream(response);
+  // Respond with the stream
+  return new StreamingTextResponse(stream);
+  }
+  catch(error){
+    if(error instanceof OpenAI.APIError){
+        const {name, status, headers, message} = error;
+      return NextResponse.json({
+        name,status,headers,message
+      }, {status})
+    }else {
+        console.error("An unexpected error occurred", error)
+        throw error
+    }
+  }
+}
