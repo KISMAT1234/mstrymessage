8daf406c1c98c101e85dc53b06b2378c589a9402
KISMAT1234 <kismatbohora123@gmail.com>
2024-04-24 23:00:34 +0545

    minor change

--- src/model/User.ts
+++ src/model/User.ts
@@ -68,3 +68,5 @@ const UserSchema: Schema<User> = new Schema({
 
 const UserModel = (mongoose.models.User as mongoose.Model<User>) || mongoose.model<User>("User",UserSchema)
 export default UserModel;
+
+
