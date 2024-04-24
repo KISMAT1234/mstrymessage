2b13fe84062bffbbeb3f41355d4bf1c363abd1c1
KISMAT1234 <kismatbohora123@gmail.com>
2024-04-24 23:25:02 +0545

    tosater installed and configured

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import AuthProvider from "@/context/AuthProvider";
+import { Toaster } from "@/components/ui/toaster"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +20,10 @@ export default function RootLayout({
     <html lang="en">
       <AuthProvider>
 
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Toaster/>
+        </body>
       </AuthProvider>
     </html>
   );
