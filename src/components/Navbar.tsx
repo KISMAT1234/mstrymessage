915052cf8fc2baca911915d9b586d3ce5894f970
KISMAT1234 <kismatbohora123@gmail.com>
2024-05-05 20:46:56 +0545

    Navbar used to render in every component

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,37 @@
 "use client"
 import React from 'react'
+import Link from 'next/link'
+import { useSession, signOut } from 'next-auth/react'
+import {User} from 'next-auth'
+import { Button } from './ui/button'
 
 const Navbar = () => {
+    const {data: session} = useSession()
+
+    const user: User = session?.user as User
+
   return (
-    <div>Navbar</div>
+    // <div>Navbar</div>
+    <nav className="p-4 md:p-6 shadow-md bg-gray-900 text-white">
+        <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
+            <a href="#" className="text-xl font-bold mb-4 md:mb-0">Mystry message</a>
+            {
+                session ? (
+                    <>
+                      <span className="mr-4">Welcome, {user?.username || user.email}</span>
+                      <Button onClick={()=> signOut()}  className="w-full md:w-auto bg-slate-100 text-black">Logout</Button>
+                    </>
+                ) : (
+                    <>
+                      <Link href='/sign-in'>
+                        <Button className="w-full md:w-auto bg-slate-100 text-black">Login</Button>
+                      </Link>
+                    </>
+                )
+            }
+        </div>
+    </nav>
+
   )
 }
 
