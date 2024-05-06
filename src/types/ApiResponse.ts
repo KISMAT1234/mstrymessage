2b15b6e4cab61c3371fa4c3242ddecf964993d77
KISMAT1234 <kismatbohora123@gmail.com>
2024-05-06 21:39:29 +0545

    accepting message customized

--- src/types/ApiResponse.ts
+++ src/types/ApiResponse.ts
@@ -2,7 +2,7 @@ import { Message } from "@/model/User";
 export interface ApiResponse{
     success:boolean;
     message:string;
-    isAcceptingMessages?: boolean;
+    isAcceptingMessage?: boolean;
     messages?: Array<Message>
 
 }
