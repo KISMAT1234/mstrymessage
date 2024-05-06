ab0f58522dcbd60108d123e23be3e14d5069bd84
KISMAT1234 <kismatbohora123@gmail.com>
2024-05-06 12:34:08 +0545

    form configured for accepting messages

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,6 +1,35 @@
-import React from 'react'
+"use client"
+import { useToast } from '@/components/ui/use-toast'
+import { Message } from '@/model/User'
+import { AcceptMessageSchema } from '@/schemas/acceptMessageSchema'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useSession } from 'next-auth/react'
+import React, { useState } from 'react'
+import { useForm } from 'react-hook-form'
 
 const page = () => {
+  const [messages, setMessages] = useState<Message[]>([])
+  const [isLoading, setIsLoading] = useState(false)
+  const [isSwitchLoading, setIsSwitchLoading] = useState(false);
+  const {toast} = useToast()
+
+  const handleDeleteMessage = (messageId: string) => {
+       setMessages(messages.filter((message)=> message._id !== messageId))
+  } 
+
+  const {data: session} = useSession();
+
+  const form = useForm({
+    resolver: zodResolver(AcceptMessageSchema)
+  })
+
+  const {register,watch, setValue} = form
+
+  const acceptMessages = watch('acceptMessages')
+
+  
+
+
   return (
     <div>Dashboard</div>
   )
