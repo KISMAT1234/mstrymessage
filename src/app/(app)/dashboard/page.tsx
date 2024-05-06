93431091d6e99937f3b7e9083c3792532ad451c0
KISMAT1234 <kismatbohora123@gmail.com>
2024-05-06 21:42:33 +0545

    fetching message through api of users

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -2,9 +2,11 @@
 import { useToast } from '@/components/ui/use-toast'
 import { Message } from '@/model/User'
 import { AcceptMessageSchema } from '@/schemas/acceptMessageSchema'
+import { ApiResponse } from '@/types/ApiResponse'
 import { zodResolver } from '@hookform/resolvers/zod'
+import axios, { AxiosError } from 'axios'
 import { useSession } from 'next-auth/react'
-import React, { useState } from 'react'
+import React, { useCallback, useState } from 'react'
 import { useForm } from 'react-hook-form'
 
 const page = () => {
@@ -27,7 +29,22 @@ const page = () => {
 
   const acceptMessages = watch('acceptMessages')
 
-  
+  const fetchAcceptMessage = useCallback(async () => {
+    setIsSwitchLoading(true)
+    try{
+      const response = await axios.get('/api/accept-messages')
+      setValue('acceptMessages', response.data.isAcceptingMessage)
+    }catch(error){
+      const axiosError = error as AxiosError<ApiResponse>;
+      toast({
+        title:"Error",
+        description: axiosError.response?.data.message || "Failed to fetch message settings",
+        variant: "destructive"
+      })
+    } finally {
+      setIsSwitchLoading(false)
+    }
+  },[])
 
 
   return (
