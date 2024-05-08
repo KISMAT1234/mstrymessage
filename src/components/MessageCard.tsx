6994973ab6cf3719b9a5b5467bc99113b8eb567a
KISMAT1234 <kismatbohora123@gmail.com>
2024-05-08 14:50:20 +0545

    async await implement

--- src/components/MessageCard.tsx
+++ src/components/MessageCard.tsx
@@ -25,22 +25,23 @@ import { X } from 'lucide-react'
 import { useToast } from './ui/use-toast'
 import axios from 'axios'
 import { ApiResponse } from '@/types/ApiResponse'
+import { Message } from '@/model/User'
   
 
 type MessageCardProps = {
   message: Message;
   onMessageDelete: (messageId: string) => void;
 }
-const MessageCard = ({message, onMessageDelete}: MessageCardProps) => {
+export const MessageCard = ({message, onMessageDelete}: MessageCardProps) => {
     const {toast} = useToast();
-    const handleDeleteConfirm = () => {
-        const response = axios.delete<ApiResponse>(`/api/delete-message/${message._id}`)
+
+    const handleDeleteConfirm = async() => {
+        const response =await axios.delete<ApiResponse>(`/api/delete-message/${message._id}`)
         toast({
             title: response.data.message
         })
         onMessageDelete(message._id)
     }
-}
 
   return (
      <>
@@ -79,5 +80,3 @@ const MessageCard = ({message, onMessageDelete}: MessageCardProps) => {
     </>
   )
 }
-
-export default MessageCard
