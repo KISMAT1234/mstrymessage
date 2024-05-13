818dc536666bfdd6078e1e572552b14048b7ba26
KISMAT1234 <kismatbohora123@gmail.com>
2024-05-13 18:13:03 +0545

    console check

--- src/app/api/check-username-unique/route.ts
+++ src/app/api/check-username-unique/route.ts
@@ -12,18 +12,15 @@ export async function GET(request: Request){
     // localhost:3000/api/check?username=elonMusk
     await dbConnect()
     try{
-      const {searchParams} = new URL(request.url)  // Getting query parameters from url to check username
+      const {searchParams} = new URL(request.url)  // We just have to convert the req.url which is a string, into a url object using new URL() with req.url as the input
     //   console.log(searchParams,'getting uername query') // output = { 'username' => 'kismat' }
       const queryParam = {
         username: searchParams.get('username')  
-
       }
     //   console.log(queryParam,' query param') // output = { username: 'kismat' }
-
     //   Validate with zod
      const result = UsernameQuerySchema.safeParse(queryParam) //safeParse checks if queryParam meets the criteria specified in UsernameQuerySchema.
     //  console.log(result,'result of validation');  //output = { success: true, data: { username: 'kismat' } }
-
      if(!result.success){
         const usernameErrors = result.error.format().username?._errors || []
         return Response.json({
@@ -34,13 +31,10 @@ export async function GET(request: Request){
             status:400
         })
      }
-
      const {username} = result.data
     //  console.log(username,'username value');  // output = kismat
-
      const existingVerifiedUser = await UserModel.findOne({username, isVerified: true})
     //  console.log( existingVerifiedUser,'user checking' )
-
      if(existingVerifiedUser){
         return Response.json({
             success: false,
