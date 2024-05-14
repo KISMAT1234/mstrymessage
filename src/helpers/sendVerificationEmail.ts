305b49ea92660ea7cd3bd1dceb3ba5ddff985a8d
KISMAT1234 <kismatbohora123@gmail.com>
2024-05-14 19:37:34 +0545

    return type declare

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -6,7 +6,7 @@ export async function sendVerificationEmail(
     email: string,
     username:string,
     verifyCode: string
-): Promise<ApiResponse> {
+): Promise<ApiResponse> {   // return type implement
     try{
         await resend.emails.send({
             from: 'you@example.com',
