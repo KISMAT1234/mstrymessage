29e8378d3e86c1af8da056099c8ebbb442fd3f5b
KISMAT1234 <kismatbohora123@gmail.com>
2024-05-14 19:37:49 +0545

    meaning setup

--- src/types/ApiResponse.ts
+++ src/types/ApiResponse.ts
@@ -1,5 +1,5 @@
 import { Message } from "@/model/User";
-export interface ApiResponse{
+export interface ApiResponse{   // An interface in TypeScript is like a blueprint for an object, specifying what properties the object should have and their types.
     success:boolean;
     message:string;
     isAcceptingMessage?: boolean;
