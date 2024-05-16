7da8fbc5d03a715d55d0e6e870d530d63fc6d766
KISMAT1234 <kismatbohora123@gmail.com>
2024-05-16 18:23:53 +0545

    minor change

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth/next";
 import { authOptions } from "./option";
 
-const handler = NextAuth(authOptions)
+const handler = NextAuth(authOptions) //This line creates an authentication handler using NextAuth and passes in your authOptions. The authOptions include settings like OAuth providers (e.g., Google, Facebook), database connections, session management details, and more.
 
-export {handler as GET, handler as POST}
+export {handler as GET, handler as POST}  //This means that when your Next.js app receives a GET or POST request related to authentication, it will use the handler function created by NextAuth with your specified options (authOptions).
