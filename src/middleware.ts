1aab984904a73404efc83c1b1786c62b87ca2fa9
KISMAT1234 <kismatbohora123@gmail.com>
2024-05-16 18:23:37 +0545

    dashboard checked for user

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,7 @@ import { getToken } from 'next-auth/jwt';
 export { default } from 'next-auth/middleware';
 
 export const config = {
-  matcher: ['/dashboard/:path*', '/sign-in', '/sign-up', '/', '/verify/:path*'],
+  matcher: ['/dashboard/:path*', '/sign-in', '/sign-up', '/', '/verify/:path*'],  // The config object specifies a list of paths (matcher) where this middleware should be applied. These paths include routes for the dashboard, sign-in, sign-up, verification, and the home page.
 };
 
 export async function middleware(request: NextRequest) {
@@ -17,12 +17,12 @@ export async function middleware(request: NextRequest) {
     (url.pathname.startsWith('/sign-in') ||
       url.pathname.startsWith('/sign-up') ||
       url.pathname.startsWith('/verify') ||
-      url.pathname === '/')
+      url.pathname === '/')             // If a token exists and the user tries to access certain routes (/sign-in, /sign-up, /verify, or /), the middleware redirects them to the dashboard (/dashboard).
   ) {
     return NextResponse.redirect(new URL('/dashboard', request.url));
   }
 
-  if (!token && url.pathname.startsWith('/dashboard')) {
+  if (!token && url.pathname.startsWith('/dashboard')) {  // If there's no token and the user tries to access the dashboard route (/dashboard), they are redirected to the sign-in page (/sign-in).
     return NextResponse.redirect(new URL('/sign-in', request.url));
   }
 
