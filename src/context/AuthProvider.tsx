07e390394cf50fcdd1036b0021730a87ba0ff740
KISMAT1234 <kismatbohora123@gmail.com>
2024-05-16 19:19:59 +0545

    minor change

--- src/context/AuthProvider.tsx
+++ src/context/AuthProvider.tsx
@@ -1,13 +1,12 @@
 "use client"
-
 import { SessionProvider } from "next-auth/react"
 
 export default function AuthProvider({
   children,
-}: {children: React.ReactNode}) {
+}: {children: React.ReactNode}) {  // This line exports a React component named AuthProvider. It takes a prop called children, which represents the content inside the component.
   return (
     <SessionProvider>
     {children}
-    </SessionProvider>
+    </SessionProvider>  // This line returns the SessionProvider component with children inside it. The SessionProvider component manages the authentication state and provides session information to your application.
   )
 }
