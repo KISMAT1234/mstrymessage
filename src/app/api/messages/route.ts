4a3af99d747ec70dc4b208c12693042f955a089b
KISMAT1234 <kismatbohora123@gmail.com>
2024-05-26 12:43:38 +0545

    getting user session

--- src/app/api/messages/route.ts
+++ src/app/api/messages/route.ts
@@ -1,13 +1,27 @@
 import dbConnect from "@/lib/dbConnect";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/option";
+import { User } from "next-auth";
+import UserModel from "@/model/User";
 
-export async function POST(request: Request){
+export async function POST(request: Request){  // Accepting messages
     await dbConnect()
-    try{
+        const session = await getServerSession(authOptions)
+        console.log(session,"session accept mesage")
+        const user: User = session?.user as User
+        console.log(user,"user check")
+        
+        if(!session || !session.user){
+            return Response.json(
+                {
+                    success: false,
+                    message: "Not Authenticated"
+                },
+                {status: 401}
+            )
+        }
 
-    }
-    catch(error){
-        console.log("Error adding message",error)
-    }
+
 }
 
 export async function GET(request: Request){
