73345422d80131fd14a4c2f5122ee73383b78da3
KISMAT1234 <kismatbohora123@gmail.com>
2024-05-28 14:25:47 +0545

    deleteing delete route

--- src/app/api/messages/route.ts
+++ src/app/api/messages/route.ts
@@ -129,57 +129,6 @@ export async function POST(request: Request){
 
 
 
-export async function DELETE(request: Request, {params}: {params: {messageid: string}}){
-    const messageId = params.messageid
-    
-    await dbConnect()
-    const session = await getServerSession(authOptions)
-    const user: User = session?.user as User
-    
-    if(!session || !session.user){
-        return Response.json({
-            success: false,
-            message: "Not Authenticated"
-        },
-        {
-            status: 401
-        })
-
-    }
-    try{
-       const updateResult = await UserModel.updateOne({
-        _id: user._id
-       },
-       {
-        $pull:{messages:{_id: messageId}} //The $pull operator is used to remove elements from an array that match a specified condition. Here's a breakdown of the code:
-       }) 
-       if(updateResult.modifiedCount == 0){
-        return Response.json({
-            success: false,
-            message: "Message not found or already deleted"
-        },
-        {
-            status: 404
-        })
-       }
-       return Response.json({
-        success: true,
-        message: "Message deleted"
-       },
-       {
-        status: 200
-       })
-    }catch(error){
-        console.log("Error in delete message route",error)
-      return Response.json({
-          success: false,
-          message: "Error deleting message"
-      },
-      {
-        status:500
-      })
-    }
-}
 
 
 
