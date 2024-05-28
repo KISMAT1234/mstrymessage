84da3f90ceabb954077b976e440efb7000dd352a
KISMAT1234 <kismatbohora123@gmail.com>
2024-05-28 14:26:19 +0545

    message content integrate and delete route integrated

--- src/components/MessageCard.tsx
+++ src/components/MessageCard.tsx
@@ -36,7 +36,7 @@ export const MessageCard = ({message, onMessageDelete}: MessageCardProps) => {
     const {toast} = useToast();
 
     const handleDeleteConfirm = async() => {
-        const response =await axios.delete<ApiResponse>(`/api/delete-message/${message._id}`)
+        const response =await axios.delete<ApiResponse>(`/api/messages/${message._id}`)
         toast({
             title: response.data.message
         })
@@ -72,7 +72,7 @@ export const MessageCard = ({message, onMessageDelete}: MessageCardProps) => {
              <CardDescription>Card Description</CardDescription>
            </CardHeader>
            <CardContent>
-             <p>Card Content</p>
+             <p>{message.content}</p>
            </CardContent>
            
       </Card>
