3cbd91f603a7371266921dddb115644da0b5ed9d
KISMAT1234 <kismatbohora123@gmail.com>
2024-05-30 22:30:42 +0545

    adding input field to send messages

--- src/app/u/[username]/page.tsx
+++ src/app/u/[username]/page.tsx
@@ -26,11 +26,40 @@ import { messageSchema } from '@/schemas/messageSchema';
 
 
 export default function SendMessage() {
+  const form = useForm<z.infer<typeof messageSchema>>({
+    resolver: zodResolver(messageSchema),
+  });
+
+  const onSubmit = () => {
+
+  }
   return (
     <div className="container mx-auto my-8 p-6 bg-white rounded max-w-4xl">
       <h1 className="text-4xl font-bold mb-6 text-center">
         Public Profile Link
       </h1>
+
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+          <FormField
+            control={form.control}
+            name="content"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Send Anonymous Message to @</FormLabel>
+                <FormControl>
+                  <Textarea
+                    placeholder="Write your anonymous message here"
+                    className="resize-none"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </form>
+      </Form>
     </div>
   );
 }
