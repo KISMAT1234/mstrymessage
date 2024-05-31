b852433d78ac786b61a31e8400a9a6931ddc424b
KISMAT1234 <kismatbohora123@gmail.com>
2024-05-31 14:35:34 +0545

    typecheck and inclufing children in component

--- src/context/StoreProvider.tsx
+++ src/context/StoreProvider.tsx
@@ -4,10 +4,10 @@ import { store } from "@/store/store"
 import React,{ReactNode} from "react"
 import { Provider } from "react-redux"
 
-export const StoreProvider = ({children}: {children: ReactNode}) => {
-    return 
-      <Provider store={store}>
-          {children}  
-      </Provider>
-}
+interface StoreProviderProps {  //The interface specifies that the StoreProvider component will receive a single prop called children, which can be any valid React node (e.g., elements, components, strings, etc.).
+    children: React.ReactNode;
+  }
+  export const StoreProvider: React.FC<StoreProviderProps> = ({ children }) => {  //It automatically includes children in the component's props type and provides type checking and autocomplete support for functional components.
+    return <Provider store={store}>{children}</Provider>;
+  };
 
