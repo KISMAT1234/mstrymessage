4a4fd27bfe7b6337014620590ba5abcc0531d128
KISMAT1234 <kismatbohora123@gmail.com>
2024-06-02 19:49:56 +0545

    wrapping configurestore into makeStore

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,13 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit'
-import likeSlice from './features/likes/likeSlice'
+import { addLike } from './features/likes/likeSlice'
 
-export const store = configureStore({
-  reducer: {
-    like:likeSlice
-  },
-})
+export const makeStore = () => {
+  return configureStore({
+    reducer: {
+      like: addLike
+    },
+  })
+}
 
+// Infer the type of makeStore
+export type AppStore = ReturnType<typeof makeStore>
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<AppStore['getState']>
+export type AppDispatch = AppStore['dispatch']
