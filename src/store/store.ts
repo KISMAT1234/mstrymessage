e125822066a897e3a16cf4f4244444863bb59e90
KISMAT1234 <kismatbohora123@gmail.com>
2024-06-02 20:00:54 +0545

    minor change

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { addLike } from './features/likes/likeSlice'
 
-export const makeStore = () => {
+export const makeStore = () => { //Now we have a function, makeStore, that we can use to create a store instance per-request while retaining the strong type safety (if you choose to use TypeScript) that Redux Toolkit provides.
   return configureStore({
     reducer: {
       like: addLike
