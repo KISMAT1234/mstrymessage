91abdae64f414477e781168a7eedde17b855fc00
KISMAT1234 <kismatbohora123@gmail.com>
2024-06-04 21:01:49 +0545

    checking update user

--- src/app/api/accept-messages/route.ts
+++ src/app/api/accept-messages/route.ts
@@ -31,6 +31,7 @@ export async function POST(request: Request){  // Accepting messages
            {isAcceptingMessages: acceptMessages},
            {new: true}
         )
+        console.log(updatedUser,'updated user')
         
         if(!updatedUser){
             return Response.json(
