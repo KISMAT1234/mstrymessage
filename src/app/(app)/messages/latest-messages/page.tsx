f9d4c7d46db8cedd3a9e4d95e2c8a96a9c44a59b
KISMAT1234 <kismatbohora123@gmail.com>
2024-06-06 15:46:41 +0545

    minor change

--- src/app/(app)/messages/latest-messages/page.tsx
+++ src/app/(app)/messages/latest-messages/page.tsx
@@ -11,7 +11,10 @@ import {
 
  const LatestMessages= () => {
   return (
-    <div>
+    <div className="my-2 mx-2 md:mx-8 lg:mx-auto p-6 bg-white rounded w-full max-w-6xl">
+      <div>
+        
+      </div>
       <Pagination>
       <PaginationContent>
         <PaginationItem>
