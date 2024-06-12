b8e7216c9e246e7ed29c16291a189120d91c223c
KISMAT1234 <kismatbohora123@gmail.com>
2024-06-12 22:47:26 +0545

    icon added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -150,14 +150,22 @@ const Navbar = () => {
              </NavigationMenuList>
            </NavigationMenu>
          </div>
-         <div>
-            <Input type="email" placeholder="Search messages here..." className="hidden w-[60%] md:block"/>
-         </div>
-
-               
+         <div className=" hidden md:block">
+          <button>
+            <Youtube className="h-[7vh] w-[10vh] mx-2 md:w-auto bg-slate-100 text-black rounded hover:text-red-600"/>
+          </button>
+          <button>
+            <Instagram className="h-[7vh] w-[10vh] mx-2 md:w-auto bg-slate-100 text-black rounded hover:text-red-500"/>
+          </button>
+          <button>
+             <Facebook className="h-[7vh] w-[10vh] mx-2 md:w-auto bg-slate-100 text-black rounded hover:bg-blue-500"/> 
+          </button>
 
         </div>
-
+        <div>
+           <Input type="email" placeholder="Search messages here..." className="hidden w-full text-black md:block"/>
+        </div>
+      </div>
     </nav>
     
     
@@ -207,74 +215,3 @@ const ListItem = React.forwardRef<
 
 
 
-//   <header className="flex items-center justify-between px-4 py-2 bg-gray-800 text-white">
-//   <div className="flex items-center">
-//     <button className="lg:hidden mr-2" onClick={toggleSidebar}>
-//       {/* Sidebar toggle button icon */}
-//       <svg
-//         className="w-6 h-6 fill-current"
-//         viewBox="0 0 24 24"
-//         xmlns="http://www.w3.org/2000/svg"
-//       >
-//         {/* Sidebar icon */}
-//         {isOpen ? (
-//           <path
-//             fillRule="evenodd"
-//             clipRule="evenodd"
-//             d="M18 4H6a2 2 0 00-2 2v12a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2zm-1 6h-5V7h5v3zm0 2h-5v3h5v-3zm-7-2v-3H7v3h3zm0 2v3H7v-3h3z"
-//           />
-//         ) : (
-//           <path
-//             fillRule="evenodd"
-//             clipRule="evenodd"
-//             d="M4 5a2 2 0 012-2h12a2 2 0 012 2v14a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm1 0a1 1 0 011-1h10a1 1 0 011 1v2H5V5zm0 3h10v9H5V8zm0 10a1 1 0 011-1h10a1 1 0 011 1v2H5v-2z"
-//           />
-//         )}
-//       </svg>
-//     </button>
-//     {/* Logo */}
-//     <a href="/">
-//       <img src="/path/to/logo.png" alt="Logo" className="h-8" />
-//     </a>
-//   </div>
-//   {/* Navigation links */}
-//   <nav className="hidden lg:flex space-x-4">
-//     <a href="/">Home</a>
-//     <a href="/about">About</a>
-//     <a href="/contact">Contact</a>
-//   </nav>
-//   {/* Logout button for large screens */}
-//   <button className="hidden lg:block">Logout</button>
-//   {/* Sidebar */}
-//   {isOpen && (
-//     <div className="fixed inset-0 bg-green-500 bg-opacity-75 flex justify-end z-50">
-//       <div className="w-80 bg-blue-600 h-full transform translate-x-0 lg:translate-x-full transition-transform duration-900 ease-in-out">
-//         {/* Sidebar content */}
-//         <div className="flex flex-col items-start justify-between h-full p-4">
-//           <button onClick={toggleSidebar}>
-//             {/* Close icon */}
-//             <svg
-//               className="w-6 h-6 fill-current text-gray-800 mb-4"
-//               viewBox="0 0 24 24"
-//               xmlns="http://www.w3.org/2000/svg"
-//             >
-//               <path
-//                 fillRule="evenodd"
-//                 clipRule="evenodd"
-//                 d="M6.7 6.7a.75.75 0 011.06 0L12 10.94l4.24-4.24a.75.75 0 111.06 1.06L13.06 12l4.24 4.24a.75.75 0 11-1.06 1.06L12 13.06l-4.24 4.24a.75.75 0 01-1.06-1.06L10.94 12 6.7 7.76a.75.75 0 010-1.06z"
-//               />
-//             </svg>
-//           </button>
-//           {/* Navigation links */}
-//           <nav className="flex flex-col space-y-2">
-//             <a href="/">Home</a>
-//             <a href="/about">About</a>
-//             <a href="/contact">Contact</a>
-//           </nav>
-//           {/* Logout button */}
-//           <button className="mt-auto">Logout</button>
-//         </div>
-//       </div>
-//     </div>
-//   )}
-// </header>
